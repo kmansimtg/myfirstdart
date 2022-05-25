83182544e6bb85965fd11fb32fdabad8a63ff9d3
kmansimtg <mtg123mansi@gmail.com>
2022-05-25 16:04:16 +0530

    first dart

---
+++ bin/myfirstdart.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+  print('Hello Mansi!');
+  int age=70;
+  print(age);
+  String name='chunli';
+  
+  print(name);
+    List nam=['hifi','bye',7];
+    print(nam);
+    print('Enter your age');
+    
+    String ?ghj = stdin.readLineSync();
+    print('Your age is ${ghj}');
+
+}
